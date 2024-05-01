3f9b36de826e429404ff12bc3797c3afd4db7a67
Abhishek Salokhe <abhisaloke2424@gmail.com>
2024-05-01 18:09:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import UsersJson from '../assets/users.json';
+import { CommonModule } from '@angular/common';
+
+interface USERS{
+  id: number,
+  name: String,
+  username: String,
+  email: String,
+  src: String
+}
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Employee-data-app';
+
+  Users: USERS[] = UsersJson;
+  constructor(){
+    // console.log(this.Users);
+  }
 }
